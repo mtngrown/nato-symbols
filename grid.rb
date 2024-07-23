74414aa4da74d7e92b6c26290eae246e4ea10dd3
JV <mtngrown7@yahoo.com>
2024-07-22 19:03:40 -0700

    xml generated ruby grid

--- grid.rb
+++ grid.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+#!/usr/bin/env ruby
+require 'nokogiri'
+
 class Grid
   attr_accessor :width, :height, :spacing
 
@@ -9,32 +12,39 @@ class Grid
     @spacing = spacing
   end
 
-  def generate_gridlines
-    lines = ""
-
-    # Vertical lines
-    (0..@width).step(@spacing) do |x|
-      lines += %Q(<line x1="#{x}" y1="0" x2="#{x}" y2="#{height}" style="stroke:lightblue;stroke-width:1"/>\n)
+  def generate_vertical_gridlines(doc)
+    doc.g(id: 'vertical-lines') do
+      # Vertical lines
+      (0..@width).step(@spacing) do |x|
+        doc.line(x1: x, y1: 0, x2: x, y2: @height, style: 'stroke:lightblue;stroke-width:1')
+      end
     end
+  end
 
-    # Horizontal lines
-    (0..@height).step(@spacing) do |y|
-      lines += %Q(<line x1="0" y1="#{y}" x2="#{width}" y2="#{y}" style="stroke:lightblue;stroke-width:1"/>\n)
+  def generate_horizontal_gridlines(doc)
+    doc.g(id: 'horizontal-lines') do
+      # Horizontal lines
+      (0..@height).step(@spacing) do |y|
+        doc.line(x1: 0, y1: y, x2: @width, y2: y, style: 'stroke:lightblue;stroke-width:1')
+      end
     end
-
-    lines
   end
 
   def save_svg(filename)
-    File.open(filename, 'w') do |file|
-      file.puts "<svg xmlns='http://www.w3.org/2000/svg' width='#{@width}' height='#{@height}'>"
-      file.puts generate_gridlines
-      file.puts "</svg>"
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.svg(xmlns: 'http://www.w3.org/2000/svg', width: @width, height: @height) do
+        xml.g(id: 'gridlines') do
+          generate_vertical_gridlines(xml)
+          generate_horizontal_gridlines(xml)
+        end
+      end
     end
+
+    File.write(filename, builder.to_xml)
   end
 end
 
 # Example usage
 grid = Grid.new(1280, 1280, 20)
-grid.save_svg('grid.svg')
+grid.save_svg('next-war-taiwan/grid_1280.svg')
 
